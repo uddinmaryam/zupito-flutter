053d5294ad83b3e0f6ed02f7e15dbb8922a960fa
Maryam <maryamuddin711@gmail.com>
2025-06-26 09:18:10 +0545

    build for web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2818729269"
+    serviceWorkerVersion: "4202680943"
   }
 });
