7376fbc3e5bdf0534140dc81884bae7dc98126b2
Maryam <maryamuddin711@gmail.com>
2025-06-26 16:41:58 +0545

    rebuild:updated web app with dotted boundary

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3669739325"
+    serviceWorkerVersion: "2879726133"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "16224a4bbdb4b3c4ef84e5899b53285d",
+const RESOURCES = {"flutter_bootstrap.js": "c34f7d4124d2a00825779279348fe607",
 "version.json": "4e2959b63d917924db5ac7e1b568c3d8",
 "index.html": "827c6570a4e6fb6c71aa81d108cb0b34",
 "/": "827c6570a4e6fb6c71aa81d108cb0b34",
-"main.dart.js": "4a06fb07c6ee1b3102d3f4c11b373298",
+"main.dart.js": "bf9e02bf54e21bf29d3369c85ffd727e",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
