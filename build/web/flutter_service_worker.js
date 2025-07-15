acb51e030e55867554a599484888715e3a34cb57
Maryam <maryamuddin711@gmail.com>
2025-07-15 20:54:24 +0545

    last and final

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e04903810ebf8dc18e131c5dba16c3a9",
+const RESOURCES = {"flutter_bootstrap.js": "ec80937afe4ed1c6accdd6ae28cb8e97",
 "version.json": "4e2959b63d917924db5ac7e1b568c3d8",
 "index.html": "827c6570a4e6fb6c71aa81d108cb0b34",
 "/": "827c6570a4e6fb6c71aa81d108cb0b34",
-"main.dart.js": "1162d588d40adb1af07beee078dd092a",
+"main.dart.js": "162ffff565d346115bae814a109c6d1f",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
